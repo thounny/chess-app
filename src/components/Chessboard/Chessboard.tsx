900e4d5cc165ac721ac5e6b193481ff079cb5ae1
thounny <thounnykeo@gmail.com>
2022-01-26 03:31:05 -0500

    En passant function === pawn

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -12,6 +12,7 @@ export interface Piece {
     y: number;
     type: PieceType;
     team: TeamType;
+    enPassant?: boolean;
 }
 
 export enum TeamType {
@@ -127,44 +128,59 @@ function dropPiece(e: React.MouseEvent) {
         // inverse y axis == switch to Math.ceil
         const y = Math.abs(Math.ceil((e.clientY - chessboard.offsetTop - 800) / 100));
 
-        const currentPiece = pieces.find(p => p.x === gridX && p.y === gridY);
-        const attackedPiece = pieces.find(p => p.x === x && p.y === y);
+        const currentPiece = pieces.find((p) => p.x === gridX && p.y === gridY);
+        const attackedPiece = pieces.find((p) => p.x === x && p.y === y);
 
         if(currentPiece) {
             const validMove = referee.isValidMove(gridX, gridY, x, y, currentPiece.type, currentPiece.team, pieces);
 
-            if(validMove) {
+            const isEnPassantMove = referee.isEnPassantMove(gridX, gridY, x, y, currentPiece.type, currentPiece.team, pieces);
+
+            const pawnDirection = currentPiece.team === TeamType.OUR ? 1 : -1;
+
+            if(isEnPassantMove) {
+                const updatedPieces = pieces.reduce((results, piece) => {
+                    if(piece.x === gridX && piece.y === gridY) {
+                        piece.enPassant = false;
+                        piece.x = x;
+                        piece.y = y;
+                        results.push(piece);
+                    } else if(!(piece.x === x && piece.y === y - pawnDirection)) {
+                        if(piece.type === PieceType.PAWN) {
+                            piece.enPassant = false;
+                        }
+                    }
+
+                    return results;
+                }, [] as Piece[])
+
+                setPieces(updatedPieces);
+            } else if(validMove) {
             // updates the piece position
-            // if a piece is attack, remove piece
+            // if a piece is attacked, remove piece
             const updatedPieces = pieces.reduce((results, piece) => {
 
-                if(piece.x === currentPiece.x && piece.y === currentPiece.y) {
+                if(piece.x === gridX && piece.y === gridY) {
+                    if(Math.abs(gridY - y) === 2 && piece.type === PieceType.PAWN) {
+                        //special move
+                        // console.log("En Passant true")
+                        piece.enPassant = true;
+                    } else {
+                        piece.enPassant = false;
+                    }
                     piece.x = x;
                     piece.y = y;
                     results.push(piece);
                 } else if(!(piece.x === x && piece.y === y)) {
+                    if(piece.type === PieceType.PAWN) {
+                        piece.enPassant = false;
+                    }
                     results.push(piece);
                 }
                 return results;
             }, [] as Piece[]);
-            
+
             setPieces(updatedPieces);
-
-            // setPieces((value) => {
-            //     const pieces = value.reduce((results, piece) => {
-
-            //         if(piece.x === currentPiece.x && piece.y === currentPiece.y) {
-            //             piece.x = x;
-            //             piece.y = y;
-            //             results.push(piece);
-            //         } else if(!(piece.x === x && piece.y === y)) {
-            //             results.push(piece);
-            //         }
-            //         return results;
-            //     }, [] as Piece[]);
-
-            //     return pieces;
-            // });
         } else {
             // resets the piece position
             activePiece.style.position = 'relative';
