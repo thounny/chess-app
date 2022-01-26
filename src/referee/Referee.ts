ebd0367f36de29f490f8539320cde12358f55dcb
thounny <thounnykeo@gmail.com>
2022-01-26 03:31:52 -0500

    en passant function // correct position, remove piece

--- src/referee/Referee.ts
+++ src/referee/Referee.ts
@@ -21,6 +21,29 @@ export default class Referee {
         }
     }
 
+    isEnPassantMove(px: number, py: number, x: number, y:number, type: PieceType, team: TeamType,  boardState: Piece[]) {
+        const pawnDirection = team === TeamType.OUR ? 1 : -1;
+
+        //white = +1
+        //black = -1
+        if(type === PieceType.PAWN) {
+            if((x - px === -1 || x - px === 1) && y - py === pawnDirection) {
+                const piece = boardState.find(p => p.x === x && p.y === y - pawnDirection && p.enPassant);
+                if(piece) {
+                return true;
+                }
+            }
+        }
+        //if the attacking piece is a pawn [DONE]
+        //upper left / upper right || bottom left / bottom right [DONE]
+        //if a piece is above or under attacked tile [DONE]
+        //if the attacked piece has made an en passant move in the previous turn [DONE]
+
+        //put piece in correct position
+        //remove en passant piece
+        return false;
+    }
+
     isValidMove(px: number, py: number, x: number, y: number, type: PieceType, team: TeamType, boardState: Piece[]) {
         // console.log("Referee is checking the move...");
         // console.log(`Previous location: (${px}, ${py})`);
