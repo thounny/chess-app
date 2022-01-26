17584f9364135cbf438ba83b2fcb443dda1f9edf
thounny <thounnykeo@gmail.com>
2022-01-25 20:32:53 -0500

    exporting Piece function

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -6,7 +6,7 @@ import Referee from "../../referee/Referee";
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
-interface Piece {
+export interface Piece {
     image: string;
     x: number;
     y: number;
@@ -131,7 +131,7 @@ function dropPiece(e: React.MouseEvent) {
         setPieces(value => {
             const pieces = value.map( p => {
                 if(p.x === gridX && p.y === gridY) {
-                    const validMove = referee.isValidMove(gridX, gridY, x, y, p.type, p.team);
+                    const validMove = referee.isValidMove(gridX, gridY, x, y, p.type, p.team, value);
 
                     if(validMove) {
                         p.x = x;
