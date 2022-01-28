351b62e93a20cf3f730fb9208833288c404a0e6b
thounny <thounnykeo@gmail.com>
2022-01-27 19:25:44 -0500

    caps lock constants

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,5 +1,7 @@
-export const verticalAxis = [ '1', '2', '3', '4', '5', '6', '7', '8' ];
-export const horizontalAxis = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' ];
+export const VERTICAL_AXIS = [ '1', '2', '3', '4', '5', '6', '7', '8' ];
+export const HORIZONTAL_AXIS = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' ];
+
+export const GRID_SIZE = 100;
 
 export interface Position {
 	x: number;
