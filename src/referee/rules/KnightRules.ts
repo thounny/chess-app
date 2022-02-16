4c616937242d8ff1b3618f25d0373ba91ca9dbf3
thounny <thounnykeo@gmail.com>
2022-02-16 01:28:17 -0500

    moved Knight movement into separate file

---
+++ src/referee/rules/KnightRules.ts
@@ -0,0 +1,26 @@
+import { Piece, Position, TeamType } from "../../Constants";
+import { tileIsEmptyOrOccupiedByOpponent } from "./GeneralRules";
+
+export const knightMove = (initialPosition: Position, desiredPosition: Position, team: TeamType, boardState: Piece[]): boolean => {
+    for (let i = -1; i < 2; i += 2) {
+        for (let j = -1; j < 2; j += 2) {
+            //top and bottom movement
+            if (desiredPosition.y - initialPosition.y === 2 * i) {
+                if (desiredPosition.x - initialPosition.x === j) {
+                    if (tileIsEmptyOrOccupiedByOpponent(desiredPosition, boardState, team)) {
+                        return true;
+                    }
+                }
+            }
+            //right and left movement
+            if (desiredPosition.x - initialPosition.x === 2 * i) {
+                if (desiredPosition.y - initialPosition.y === j) {
+                    if (tileIsEmptyOrOccupiedByOpponent(desiredPosition, boardState, team)) {
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+    return false;
+}
